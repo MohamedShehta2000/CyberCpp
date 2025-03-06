47cec7121b0d99f995d7dbb943b892ee7ca89775
Mo.shehta <mohamedshehta2024@gmail.com>
2025-03-07 01:36:47 +0200

    for

---
+++ C++_eJPTv1/08.for.cpp
@@ -0,0 +1,18 @@
+/*
+mohamed shehta 7/3/2025
+eJPT V1
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	for (a = 2; a <= 50; a = a+2)
+	{
+		cout << a << "-";
+	}
+	cin.ignore();
+	return 0;
+}
